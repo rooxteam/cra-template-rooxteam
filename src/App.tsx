14c414844c979cae6f6eb3039940faec1f01ecfd
iabdulov <iabdulov@roox.ru>
2020-03-23 10:33:03 +0300

    Small fix.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { Navbar } from './components'
@@ -9,13 +9,13 @@ import { AUTH_TOKEN_KEY } from './configs/constants'
 import { appInit } from './redux/auth/actions'
 
 const App: React.FC = () => {
-  const token = useMemo(() => getCookie(AUTH_TOKEN_KEY), [])
   const dispatch = useDispatch()
   useEffect(() => {
+    const token = getCookie(AUTH_TOKEN_KEY)
     if (token) {
       dispatch(appInit())
     }
-  }, [])
+  }, [dispatch])
 
   return (
     <BrowserRouter>
